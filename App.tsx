379fca3fa4fa4fa3fa175d474fa02dd2aabf8a79
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-05-08 18:50:59 +0200

    Added firebaseConfig

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import * as firebase from "firebase";
+import firebaseConfigJson from "./firebaseConfig.json";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+if (firebase.default.apps.length === 0) {
+  firebase.default.initializeApp(firebaseConfigJson);
+}
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
