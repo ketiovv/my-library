e0bde1f00a3df2dad35dcf0833d0eaec6567e5e3
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-05-11 23:29:37 +0200

    Working on auth flow

--- screens/Authentication/LoginScreen.tsx
+++ screens/Authentication/LoginScreen.tsx
@@ -1,14 +1,37 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import * as React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import { AuthenticationParamList } from "../../types";
+import firebase from "firebase";
 
 const LoginScreen = ({
   navigation,
 }: StackScreenProps<AuthenticationParamList, "LoginScreen">) => {
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const handleLogin = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login page</Text>
+      <TextInput placeholder="email" onChangeText={(text) => setEmail(text)} />
+      <TextInput
+        placeholder="password"
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button onPress={handleLogin} title="Sign In" />
     </View>
   );
 };
